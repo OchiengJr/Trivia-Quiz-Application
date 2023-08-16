1b9a525b547240db9423506b81295bd2f4a78f74
OchiengJr <137082048+OchiengJr@users.noreply.github.com>
2023-08-16 14:55:04 +0000

    Fix all changes

---
+++ script.js
@@ -0,0 +1,33 @@
+const quizData = [
+    {
+
+    Question: Which is the capital city of Australia?
+    a: "Sydney",
+    b: "Canberra",
+    c: "Melbourne",
+    d: "Perth",
+    Correct: "b",
+    },
+{
+    Question: Which country invented tea?
+    a: "Kenya",
+    b: "India",
+    c: "China",
+    d: "Japan",
+    Correct: "c",
+
+},
+{
+    Question: Which of the following countries has no coastline?
+    a: "Botswana",
+    b: "Egypt",
+    c: "Tanzania",
+    d: "South Africa",
+    Correct: "a",
+},
+
+];
+
+const quiz= document.getElementById('quiz')
+const answerEIS = document.querySelectorAll('.answer')
+const questionEI = document.getElementById('question')
